0c52fc87161251752f4b187fb98ad67c2f8e4ce7
Alberto Duran <albertoduranfilho@gmail.com>
2026-03-04 11:39:29 -0300

    :wrench: chore: Configuração de pastas e arquivos do projeto.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-8xl text-amber-900">DEIXAR COMO ESTAVA</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
