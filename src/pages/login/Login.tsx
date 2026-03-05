4dfcd5ce0ba8a27d0f043b73dd9b3eadc84161f7
Renato Sales <renatosalespaula@gmail.com>
2026-03-05 18:38:36 -0300

    implementação da responsividade da page Login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,46 +1,43 @@
-import { useContext, useEffect, useState, type ChangeEvent, type FormEvent } from "react";
+import { useContext, useState, type ChangeEvent } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
 import { AuthContext } from "../../contexts/AuthContext";
 import type { RestauranteLogin } from "../../models/RestauranteLogin";
 
-
 function Login() {
+  const navigate = useNavigate();
 
-const navigate = useNavigate();
+  const { restaurante, handleLogin, isLoading } = useContext(AuthContext);
 
-const { restaurante, handleLogin, isLoading } = useContext(AuthContext)
-
-const [restauranteLogin, setUsuarioLogin] = useState<RestauranteLogin>(
+  const [restauranteLogin, setUsuarioLogin] = useState<RestauranteLogin>(
     {} as RestauranteLogin
-)
+  );
 
-/*useEffect(() => {
+  /*useEffect(() => {
     if (restaurante.token !== "") {
         navigate('/home')
     }
-}, [restaurante])*/ 
+}, [restaurante])*/
 
-function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
     setUsuarioLogin({
-        ...restauranteLogin,
-        [e.target.name]: e.target.value
-    })
-}
+      ...restauranteLogin,
+      [e.target.name]: e.target.value,
+    });
+  }
 
-function login(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    handleLogin(restauranteLogin)
-}
-
-
+  function login(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    handleLogin(restauranteLogin);
+  }
 
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold  shadow-2xl bg-slate-100 ">
-        <form className="flex justify-center items-center flex-col w-1/2 gap-4 rounded-2xl bg-slate-100 "
-        onSubmit={login}>
-
+        <form
+          className="flex justify-center items-center flex-col w-[90vw] md:w-1/2 gap-4 shadow-xl rounded-xl bg-white p-8 "
+          onSubmit={login}
+        >
           <h2 className="text-slate-900 text-5xl ">Entrar</h2>
           <div className="flex flex-col w-full">
             <label htmlFor="usuario">Usuário</label>
@@ -49,9 +46,11 @@ function login(e: FormEvent<HTMLFormElement>) {
               id="usuario"
               name="usuario"
               placeholder="Usuario"
-              className= "w-full border border-gray-300 rounded-md px-4 py-2.5 bg-white focus:outline-none focus:ring-2 focus:ring-[#00856F] focus:border-[#00856F] transition"
+              className="w-full border border-gray-300 rounded-md px-4 py-2.5 bg-white focus:outline-none focus:ring-2 focus:ring-[#00856F] focus:border-[#00856F] transition"
               value={restauranteLogin.usuario}
-              onChange={(e: ChangeEvent<HTMLInputElement>)=> atualizarEstado(e)}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
           <div className="flex flex-col w-full">
@@ -62,9 +61,10 @@ function login(e: FormEvent<HTMLFormElement>) {
               name="senha"
               placeholder="Senha"
               className="w-full border border-gray-300 rounded-md px-4 py-2.5 bg-white focus:outline-none focus:ring-2 focus:ring-[#00856F] focus:border-[#00856F] transition"
-              value = {restauranteLogin.senha}
-              onChange={(e: ChangeEvent<HTMLInputElement>)=> atualizarEstado(e)}
-
+              value={restauranteLogin.senha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
           <button
@@ -79,24 +79,20 @@ function login(e: FormEvent<HTMLFormElement>) {
                focus:ring-[#00856F] 
                hover:opacity-80
                transition
-               hover:cursor-pointer">
-            {isLoading ?
-            <ClipLoader
-                color="#ffffff"
-                size={24}
-                />:
-            <span>Entrar</span>
-}
+               hover:cursor-pointer"
+          >
+            {isLoading ? (
+              <ClipLoader color="#ffffff" size={24} />
+            ) : (
+              <span>Entrar</span>
+            )}
           </button>
 
           <hr className="border-slate-800 w-full" />
 
-          <p>
+          <p className="text-sm">
             Ainda não tem uma conta?{" "}
-            <Link
-              to="/cadastro"
-              className="text-[#00856F] hover:underline"
-            >
+            <Link to="/cadastro" className="text-[#00856F] hover:underline">
               Cadastre-se
             </Link>
           </p>
@@ -111,12 +107,11 @@ function login(e: FormEvent<HTMLFormElement>) {
                 justify-center 
                 text-white 
                 p-12"
-                >
-                  <h1 className="text-white text-4xl font-bold text-center px-10">
-                  Bem-vindo ao Nutrihub
-                  </h1>
-          </div>
-        
+        >
+          <h1 className="text-white text-4xl font-bold text-center px-10">
+            Bem-vindo ao Nutrihub
+          </h1>
+        </div>
       </div>
     </>
   );
