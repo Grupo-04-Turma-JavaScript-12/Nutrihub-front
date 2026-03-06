1968105d3723a84043ed6db1275e9e2980c58902
Renato Sales <renatosalespaula@gmail.com>
2026-03-06 17:25:00 -0300

    implementação do componente FormCategoria

--- src/components/categoria/formcategoria/FormCategoria.tsx
+++ src/components/categoria/formcategoria/FormCategoria.tsx
@@ -1,12 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable react-hooks/set-state-in-effect */
 import { useContext, useEffect, useState, type ChangeEvent } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-// import { ClipLoader } from "react-spinners";
+import { ClipLoader } from "react-spinners";
+import { AuthContext } from "../../../contexts/AuthContext";
 import type Categoria from "../../../models/Categoria";
 import { atualizar, buscar, cadastrar } from "../../../services/Service";
 import { ToastAlerta } from "../../../utils/ToastAlerta";
-import { AuthContext } from "../../../contexts/AuthContext";
+import ListCategorias from "../listcategoria/ListCategorias";
 
 function FormCategoria() {
   const navigate = useNavigate();
@@ -14,18 +13,15 @@ function FormCategoria() {
   const [categoria, setCategoria] = useState<Categoria>({} as Categoria);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const { usuario, handleLogout } = useContext(AuthContext);
-  const token = usuario.token;
+  const { restaurante, handleLogout } = useContext(AuthContext);
+  const token = restaurante.token;
 
   const { id } = useParams<{ id: string }>();
 
   async function buscarPorId(id: string) {
     try {
-      setIsLoading(true);
       await buscar(`/categorias/${id}`, setCategoria, {
-        headers: {
-          Authorization: token,
-        },
+        headers: { Authorization: token },
       });
     } catch (error: any) {
       if (error.toString().includes("401")) {
@@ -36,12 +32,12 @@ function FormCategoria() {
 
   useEffect(() => {
     if (token === "") {
-      ToastAlerta("Você precisa estar logado!",'info');
+      ToastAlerta("Você precisa estar logado!", "info");
       navigate("/");
     }
   }, [token]);
 
-    useEffect(() => {
+  useEffect(() => {
     if (id !== undefined) {
       buscarPorId(id);
     }
@@ -58,7 +54,7 @@ function FormCategoria() {
     navigate("/categorias");
   }
 
-  async function gerarNovoCategoria(e: ChangeEvent<HTMLFormElement>) {
+  async function gerarNovaCategoria(e: ChangeEvent<HTMLFormElement>) {
     e.preventDefault();
     setIsLoading(true);
 
@@ -67,12 +63,12 @@ function FormCategoria() {
         await atualizar(`/categorias`, categoria, setCategoria, {
           headers: { Authorization: token },
         });
-        ToastAlerta("A Categoria foi atualizada com sucesso!", 'sucesso');
+        ToastAlerta("A categoria foi atualizada com sucesso!", "sucesso");
       } catch (error: any) {
         if (error.toString().includes("401")) {
           handleLogout();
         } else {
-          ToastAlerta("Erro ao atualizar a categoria.",'erro');
+          ToastAlerta("Erro ao atualizar a categoria.", "erro");
         }
       }
     } else {
@@ -80,12 +76,12 @@ function FormCategoria() {
         await cadastrar(`/categorias`, categoria, setCategoria, {
           headers: { Authorization: token },
         });
-        ToastAlerta(" A categoria foi cadastrada com sucesso!",'sucesso');
+        ToastAlerta("A categoria foi cadastrada com sucesso!", "sucesso");
       } catch (error: any) {
         if (error.toString().includes("401")) {
           handleLogout();
         } else {
-          ToastAlerta("Erro ao cadastrar a categoria.",'erro');
+          ToastAlerta("Erro ao cadastrar a categoria.", "erro");
         }
       }
     }
@@ -95,50 +91,75 @@ function FormCategoria() {
   }
 
   return (
-    <div className="min-h-[70vh] flex items-center justify-center px-4">
-      <div className="w-full max-w-xl bg-white rounded-xl shadow-lg p-8">
-        <h1 className="text-2xl font-semibold text-gray-800 text-center mb-6">
-          {id === undefined ? "Cadastrar Categoria" : "Editar Categoria"}
+    <section>
+      <div className="pt-15">
+        <h1 className="text-4xl font-bold text-center py-4 md:text-4xl">
+          Categorias
         </h1>
-
-        <form className="flex flex-col gap-6" onSubmit={gerarNovoCategoria}>
-          <div className="flex flex-col gap-2">
-            <label
-              htmlFor="descricao"
-              className="text-sm font-medium text-gray-700"
-            >
-              Descrição da Categoria
+      </div>
+      <div className="md:container md:mx-auto md:py-4 p-4 flex flex-col gap-4 md:gap-0 border-t-[0.5px] border-gray-300">
+        <h2 className="text-3xl text-center">
+          {id === undefined ? "Cadastrar Categoria" : "Editar Categoria"}
+        </h2>
+        <form
+          className="w-full flex flex-col gap-4"
+          onSubmit={gerarNovaCategoria}
+        >
+          <div className="flex flex-col">
+            <label htmlFor="nome" className="font-semibold">
+              Nome:
             </label>
-
             <input
               type="text"
-              id="descricao"
-              name="descricao"
-              placeholder="Ex: Medicamentos, Higiene, Vitaminas..."
-              className="border border-gray-300 rounded-lg px-4 py-2
-                         focus:outline-none focus:ring-2 focus:ring-green-500"
-              value={categoria.descricao || ""}
-              onChange={atualizarEstado}
-              required
+              placeholder="Digite o nome da categoria"
+              name="nome"
+              id="nome"
+              className="border-2 border-slate-700 rounded p-2"
+              value={categoria.nome}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
             />
           </div>
-
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="bg-green-600 hover:bg-green-700 text-white
-                       rounded-lg py-3 font-semibold transition-colors
-                       flex justify-center items-center"
-          >
-            {isLoading ? (
-              <ClipLoader color="#ffffff" size={22} />
-            ) : (
-              <span>{id === undefined ? "Cadastrar" : "Atualizar"}</span>
-            )}
-          </button>
+          <div className="flex flex-col w-full">
+            <label htmlFor="descricao" className="font-semibold">
+              Descrição:
+            </label>
+            <input
+              type="text"
+              placeholder="Descrição da sua categoria"
+              name="descricao"
+              id="descricao"
+              className="border-2 border-slate-700 rounded p-2"
+              value={categoria.descricao}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
+            />
+          </div>
+          <div className="flex justify-end md:justify-center gap-4 md:py-4">
+            <button className="bg-[#00856F] rounded-2xl w-full text-white font-semibold py-2 md:w-[15vw] hover:cursor-pointer hover:bg-[#044d40] transition-all duration-300">
+              {isLoading ? (
+                <ClipLoader color="#ffffff" size={24} />
+              ) : (
+                <span>
+                  {id === undefined
+                    ? "Cadastrar categoria"
+                    : "Atualizar categoria"}
+                </span>
+              )}
+            </button>
+          </div>
         </form>
+        <div className="md:grid md:grid-cols-3 md:px-4 text-gray-400 font-semibold hidden">
+          <p className="md:hidden">Nome</p>
+          <p className="md:hidden">Descrição</p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <ListCategorias />
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
