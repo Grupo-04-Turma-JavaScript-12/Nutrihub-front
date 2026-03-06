f2e45e1896329c9d005714a6292233190a730a8a
Renato Sales <renatosalespaula@gmail.com>
2026-03-06 17:56:54 -0300

    modificações no componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import DeletarCategoria from "./components/categoria/deletarcategoria/DeletarCategoria";
 import FormCategoria from "./components/categoria/formcategoria/FormCategoria";
-import ListaCategoria from "./components/categoria/listacategoria/ListaCategoria";
-
+import ListaCategorias from "./components/categoria/listacategoria/ListaCategoria";
+import Navbar from "./components/navbar/Navbar";
+import { AuthProvider } from "./contexts/AuthContext";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/Home/Home";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <div>
-        <Routes>
-        <Route path="/editarcategoria/:id" element={<FormCategoria />} />
-        <Route path="/categorias" element={<ListaCategoria />} />
-        <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
-        {/* <h1 className="text-8xl text-amber-900">DEIXAR COMO ESTAVA</h1> */}
-        </Routes>
-      </div>
-      </BrowserRouter>
+      <AuthProvider>
+        <ToastContainer />
+        <BrowserRouter>
+          <div className="min-h-[80vh]">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route
+                path="/deletarcategoria/:id"
+                element={<DeletarCategoria />}
+              />
+              <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+              {/* <h1 className="text-8xl text-amber-900">DEIXAR COMO ESTAVA</h1> */}
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
