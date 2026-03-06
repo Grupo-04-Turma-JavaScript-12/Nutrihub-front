1ce2c29c24defbfe503056a37045769ba4670fe6
Renato Sales <renatosalespaula@gmail.com>
2026-03-06 17:32:24 -0300

    ajustes na model Categoria

--- src/models/Categoria.ts
+++ src/models/Categoria.ts
@@ -1,7 +1,8 @@
-import type  Refeicao  from "./Refeicao";
+import type { Refeicao } from "./Refeicao";
+
 export default interface Categoria {
   id: number;
-  nome:string;
+  nome: string;
   descricao: string;
   refeicao?: Refeicao[] | null;
 }
