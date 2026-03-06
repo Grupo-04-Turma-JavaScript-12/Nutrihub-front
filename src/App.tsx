892f9a740bc4f6dbd097fa68e526d8b55cb99fc0
Renato Sales <renatosalespaula@gmail.com>
2026-03-06 18:21:38 -0300

    modificações no componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Cadastro from "./pages/cadastro/Cadastro";
-import Login from "./pages/login/Login";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <div>
-          <Routes>
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <div>
+        <h1 className="text-8xl text-amber-900">DEIXAR COMO ESTAVA</h1>
+      </div>
     </>
   );
 }
