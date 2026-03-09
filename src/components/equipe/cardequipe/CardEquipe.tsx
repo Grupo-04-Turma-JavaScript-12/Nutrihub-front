2d584e0e9478f539789bdcf67284340f9f44428d
Alberto Duran <albertoduranfilho@gmail.com>
2026-03-09 07:34:21 -0300

    :sparkles: feat: Fazendo ajustes no card plano e equipe.

--- src/components/equipe/cardequipe/CardEquipe.tsx
+++ src/components/equipe/cardequipe/CardEquipe.tsx
@@ -1,46 +1,53 @@
 import { GithubLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react";
 
 interface CardEquipeProps {
-    membro: {
-        name: string;
-        cargo: string;
-        image: string;
-        github: string;
-        linkedin: string;
-    };
+  membro: {
+    name: string;
+    cargo: string;
+    image: string;
+    github: string;
+    linkedin: string;
+  };
 }
 
 function CardEquipe({ membro }: CardEquipeProps) {
-    return (
-        <>
-                <div className="border border-slate-200 rounded-lg shadow-sm md:pt-4">
-                    <img
-                        src={membro.image}
-                        alt="Foto do membro da equipe"
-                        className="w-full h-auto rounded-full mx-auto sm:w-52 p-4 md:p-0" />
-                    <div className="p-4 w-full h-auto">
-                        <div>
-                            <p className="font-bold">{membro.name}</p>
-                            <p className="mb-2 text-[#00856F] font-bold text-sm">{membro.cargo}</p>
-                        </div>
-                        <div className="w-full h-auto flex flex-col md:flex-row gap-4 pt-4 text-[#00856F] font-bold">
-                            <a href={membro.linkedin}
-                                target="_black" className="flex flex-1 gap-1 items-center justify-center bg-[#DCE9E2] rounded p-2">
-                                <LinkedinLogoIcon
-                                size={28}
-                                weight="bold" />
-                                LinkedIn
-                            </a>
-                            <a href={membro.github}
-                                target="_black" className="flex flex-1 gap-1 items-center justify-center bg-[#DCE9E2] rounded p-2">
-                                <GithubLogoIcon size={28} weight="bold" />
-                                GitHub
-                            </a>
-                        </div>
-                    </div>
-                </div>
-        </>
-    );
+  return (
+    <>
+      <div className="border border-slate-200 rounded-lg shadow-sm md:pt-4">
+        <img
+          src={membro.image}
+          alt="Foto do membro da equipe"
+          className="w-full h-auto rounded-full mx-auto sm:w-52 p-4 md:p-0"
+        />
+        <div className="p-4 w-full h-auto">
+          <div>
+            <p className="font-bold">{membro.name}</p>
+            <p className="mb-2 text-[#00856F] font-bold text-sm">
+              {membro.cargo}
+            </p>
+          </div>
+          <div className="w-full h-auto flex flex-col md:flex-row gap-4 pt-4 text-[#00856F] font-bold">
+            <a
+              href={membro.linkedin}
+              target="_black"
+              className="flex flex-1 gap-1 items-center justify-center bg-[#DCE9E2] transition duration-300 hover:bg-[#ebebeb] rounded p-2"
+            >
+              <LinkedinLogoIcon size={28} weight="bold" />
+              LinkedIn
+            </a>
+            <a
+              href={membro.github}
+              target="_black"
+              className="flex flex-1 gap-1 items-center justify-center bg-[#DCE9E2] transition duration-300 hover:bg-[#ebebeb] rounded p-2"
+            >
+              <GithubLogoIcon size={28} weight="bold" />
+              GitHub
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CardEquipe;
--- src/components/equipe/listaequipe/ListaEquipe.tsx
+++ src/components/equipe/listaequipe/ListaEquipe.tsx
@@ -1,46 +1,45 @@
 import CardEquipe from "../cardequipe/CardEquipe";
 
 function ListaEquipe() {
-
   const equipe = [
     {
       name: "Alberto Durán",
-      cargo: "Tech Lead",
+      cargo: "Desenvolvedor Full Stack",
       image: "https://avatars.githubusercontent.com/u/67593467?v=4",
       linkedin: "https://www.linkedin.com/in/alberto-janeiro",
       github: "https://github.com/AlbertoDuranFilho",
     },
     {
       name: "André César",
-      cargo: "Tech Lead",
+      cargo: "Desenvolvedor Full Stack",
       image: "https://avatars.githubusercontent.com/u/216065214?v=4",
       linkedin: "https://www.linkedin.com/in/andrecesar-dev/",
       github: "https://github.com/andrecesarhdev",
     },
     {
       name: "Bruna Melo",
-      cargo: "Tech Lead",
+      cargo: "Desenvolvedor Full Stack",
       image: "https://avatars.githubusercontent.com/u/226072233?v=4",
       linkedin: "https://www.linkedin.com/in/bruna-melo-9335381b3/",
       github: "https://github.com/bkmelo",
     },
     {
       name: "Giovanna Roberta",
-      cargo: "Tech Lead",
+      cargo: "Desenvolvedor Full Stack",
       image: "https://avatars.githubusercontent.com/u/102972472?v=4",
       linkedin: "https://www.linkedin.com/in/giovannaroberta/",
       github: "https://github.com/gioroberta",
     },
     {
       name: "Jacqueline Cardeal",
-      cargo: "Tech Lead",
+      cargo: "Desenvolvedor Full Stack",
       image: "https://avatars.githubusercontent.com/u/141357140?v=4",
       linkedin: "https://www.linkedin.com/in/jacqueline-cardeal/",
       github: "https://github.com/jackmarques",
     },
     {
       name: "Renato Sales",
-      cargo: "Tech Lead",
+      cargo: "Desenvolvedor Full Stack",
       image: "https://avatars.githubusercontent.com/u/101156709?v=4",
       linkedin: "https://www.linkedin.com/in/renato-sales-desenvolvedor/",
       github: "https://github.com/renato-sales",
@@ -49,7 +48,9 @@ function ListaEquipe() {
 
   return (
     <>
-      <h1 className="text-center text-3xl font-bold mb-12 pt-10">Conheça Nossa <span className="block md:inline">Equipe</span></h1>
+      <h1 className="text-center text-3xl font-bold mb-12 pt-10">
+        Conheça Nossa <span className="block md:inline">Equipe</span>
+      </h1>
       <div className="bg-gray-50 px-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-6xl mx-auto md:pb-8">
           {equipe.map((membro) => (
@@ -60,6 +61,5 @@ function ListaEquipe() {
     </>
   );
 }
-
 
 export default ListaEquipe;
