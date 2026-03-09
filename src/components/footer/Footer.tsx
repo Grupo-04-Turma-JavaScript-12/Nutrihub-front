6e1e4c7d925aa1749b80bc61ec5ec9f2dd9cd236
Alberto Duran <albertoduranfilho@gmail.com>
2026-03-09 07:57:27 -0300

    :sparkles: feat: Colocando as condições para mostrar Navbar e footer.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,21 @@
+import { useLocation } from "react-router-dom";
+
 function Footer() {
+  const location = useLocation();
+
+  const rotasSemFooter = ["/login", "/cadastro"];
+
+  if (rotasSemFooter.includes(location.pathname)) {
+    return null;
+  }
+
   return (
     <footer className="bg-[#00856F] text-amber-50">
-      
       <div className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center text-center gap-2 py-6 px-6">
-        
         <h2 className="text-2xl font-semibold">NutriHub</h2>
         <p className="text-sm">© 2026 - NutriHub</p>
         <p className="text-sm">Todos os direitos reservados.</p>
-
       </div>
-
     </footer>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,30 @@
 import { ListIcon } from "@phosphor-icons/react";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom"; // Importe o useLocation
+import { AuthContext } from "../../contexts/AuthContext";
+import { ToastAlerta } from "../../utils/ToastAlerta";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation(); // Hook para pegar a rota atual
+
+  const { restaurante, handleLogout } = useContext(AuthContext);
+
+  function logout() {
+    handleLogout();
+    ToastAlerta("O Usuário foi desconectado com sucesso!", "info");
+    navigate("/");
+  }
+
   const [menuAberto, setMenuAberto] = useState(false);
+
+  // Definimos as rotas onde o Navbar NÃO deve aparecer
+  const rotasSemNavbar = ["/login", "/cadastro"];
+
+  // Se a rota atual estiver na lista acima, retornamos null (nada aparece)
+  if (rotasSemNavbar.includes(location.pathname)) {
+    return null;
+  }
 
   return (
     <header className="bg-[#00856F] w-full fixed top-0 left-0 z-50">
@@ -14,8 +35,7 @@ function Navbar() {
 
         <ul className="hidden md:flex text-amber-50 gap-10">
           <li className="hover:underline hover:cursor-pointer">
-            {" "}
-            <Link to="/home"> Home</Link>
+            <Link to="/home">Home</Link>
           </li>
           <li className="hover:underline hover:cursor-pointer">
             <Link to="/perfil">Perfil</Link>
@@ -26,18 +46,31 @@ function Navbar() {
           <li className="hover:underline hover:cursor-pointer">
             <Link to="/categorias">Categorias</Link>
           </li>
+
+          {restaurante.token !== "" && (
+            <li className="hover:underline hover:cursor-pointer">
+              <Link to="" onClick={logout}>
+                Sair
+              </Link>
+            </li>
+          )}
         </ul>
 
         <div className="hidden md:block">
-          <Link
-            to="/login"
-            className="px-6 py-2 rounded-xl border text-white font-semibold hover:bg-white hover:text-[#00856F] transition-all duration-300"
-          >
-            ENTRAR
-          </Link>
+          {restaurante.token === "" ? (
+            <Link
+              to="/login"
+              className="px-6 py-2 rounded-xl border text-white font-semibold hover:bg-white hover:text-[#00856F] transition-all duration-300"
+            >
+              ENTRAR
+            </Link>
+          ) : (
+            <span className="text-white font-medium">
+              Olá, {restaurante.nome}
+            </span>
+          )}
         </div>
 
-        {/* Icone mobile */}
         <button
           className="md:hidden text-white"
           onClick={() => setMenuAberto(!menuAberto)}
@@ -46,28 +79,28 @@ function Navbar() {
         </button>
       </nav>
 
-      {/* Menu Mobile */}
       {menuAberto && (
         <div className="md:hidden bg-[#006d5b] flex flex-col items-center gap-4 py-6 text-white">
-          <span className="hover:underline hover:cursor-pointer">
-            {" "}
-            <Link to="/home"> Home</Link>
-          </span>
-          <span className="hover:underline hover:cursor-pointer">
-            <Link to="/perfil">Perfil</Link>
-          </span>
-          <span className="hover:underline hover:cursor-pointer">
-            <Link to="/refeicoes">Refeições</Link>
-          </span>
-          <span className="hover:underline hover:cursor-pointer">
-            <Link to="/categorias">Categorias</Link>
-          </span>
-          <Link
-            to="/login"
-            className="mt-4 px-6 py-2 rounded-xl border font-semibold"
-          >
-            ENTRAR
+          <Link to="/home" onClick={() => setMenuAberto(false)}>
+            Home
           </Link>
+          <Link to="/perfil" onClick={() => setMenuAberto(false)}>
+            Perfil
+          </Link>
+          <Link to="/refeicoes" onClick={() => setMenuAberto(false)}>
+            Refeições
+          </Link>
+          <Link to="/categorias" onClick={() => setMenuAberto(false)}>
+            Categorias
+          </Link>
+          {restaurante.token === "" && (
+            <Link
+              to="/login"
+              className="mt-4 px-6 py-2 rounded-xl border font-semibold"
+            >
+              ENTRAR
+            </Link>
+          )}
         </div>
       )}
     </header>
